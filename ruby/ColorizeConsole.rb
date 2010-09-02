5ea3d90ad27184849ca50ffb5f9ab14453720a32
Mark Borcherding <markborcherding@gmail.com>
2010-09-02 12:16:18 -0500

    added green functions

--- ruby/ColorizeConsole.rb
+++ ruby/ColorizeConsole.rb
@@ -3,28 +3,28 @@ require 'rubygems'
 
 class ConsoleColor
 	
-	COLORS = { :red => 31, 
-				 :green => 21
-				}
+	COLORS = {  :red => 31, 
+				      :green => 32
+				    }
 				
 	EFFECTS = { :normal => 0,
-				  :bright => 1
-				}		
+				      :bright => 1
+			      }		
 	
 	def self.colorize(text, color_code, effect)
   		"\e[#{EFFECTS[effect]};#{COLORS[color_code]}m#{text}\e[0m"
 	end
 
 	
-	#eventually replace this with method missing 
 	def self.red(text); colorize(text, :red , :normal); end
 	def self.light_red(text); colorize(text, :red , :bright ); end
 	def self.green(text); colorize(text, :green , :normal); end
+	def self.light_green(text); colorize(text, :green , :bright); end
 	
 	
 end
 
-
-
 puts 'light red [ ' + ConsoleColor.light_red('light') + ' ]'
 puts 'dark red  [ ' + ConsoleColor.red('dark') +  '  ]'
+puts 'dark green  [ ' + ConsoleColor.green('dark') +  '  ]'
+puts 'light green  [ ' + ConsoleColor.light_green('light') +  '  ]'
