096ea0a59f32b63d7bd00b561ac006b019c70f0e
Mark Borcherding <markborcherding@gmail.com>
2010-09-02 16:22:32 -0500

    giving up on coloring the console

--- ruby/ColorizeConsole.rb
+++ ruby/ColorizeConsole.rb
@@ -1,31 +1,45 @@
 require 'rubygems'
 #require 'win32console'
 
-class ConsoleColor
+class CoolColorConsole
 	
-	COLORS = {  :red => 31, 
-				      :green => 32
-				    }
-				
-	EFFECTS = { :normal => 0,
-				      :light => 1
-			      }		
+  COLORS = {  
+    :red => 31, 
+    :green => 32,
+    :black  => 30,
+    :yellow => 33, 
+    :blue => 34, 
+    :magenta => 35, 
+    :cyan => 36, 
+    :white => 37
+  }
+  
+  EFFECTS = { 
+    :normal => 0,
+    :light => 1
+  }		
 	
-	def self.colorize(text, options = {})
-	    color = options[:color] || :green
-	    effect = options[:effect] || :normal
-  		"\e[#{EFFECTS[effect]};#{COLORS[color]}m#{text}\e[0m"
-	end
-	
-	def self.puts(text, options = {})
-	  super colorize text,options
+  def self.color(text, color = :white)
+    "\e[0;#{COLORS[color]}m#{text}\e[0m"
   end
-	
+
+  def self.bright(text)
+    "\e[1;#{text}\e[0m"
+  end
+  
 end
 
-puts 'light red [ ' + ConsoleColor.colorize('light', :color => :red, :effect => :light) + ' ]'
-puts 'dark red  [ ' + ConsoleColor.colorize('dark', :color => :red) +  '  ]'
-puts 'dark green  [ ' + ConsoleColor.colorize('dark') +  '  ]'
-puts 'light green  [ ' + ConsoleColor.colorize('light', :effect => :light) +  '  ]'
+class String
+  def color(color = :white)
+    CoolColorConsole.color(self,color)
+  end
 
-ConsoleColor.puts "Green"
+  def bright
+    CoolColorConsole.bright(self)
+  end
+end
+
+
+puts "some green".color(:green).bright + " blue".color(:blue)
+puts "some red".color(:red).bright
+puts "bright".bright
---
+++ ruby/readme.md
@@ -0,0 +1,4 @@
+# Just use rainbow
+http://sickill.net/blog/2009/03/24/colorizing-console-output-with-rainbow-ruby-gem.html
+
+`gem install rainbow`
