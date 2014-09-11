2780fcdbb7d73806b11b63e019294cb5800ad152
Mark Borcherding <markborcherding@gmail.com>
2014-09-10 21:05:48 -0500

    tweak bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+set -xe
+
 grep Ubuntu /proc/version && \
   sudo apt-get update && \
   sudo apt-get --quiet --yes install git
@@ -5,7 +7,11 @@ grep Ubuntu /proc/version && \
 [ uname == 'Darwin' ] && \
   brew install git
 
-which pip
-[ $? != 0 ] && sudo easy_install pip
+# which pip
+# [ $? != 0 ] && sudo easy_install pip
 
-sudo pip install paramiko PyYAML jinja2 httplib2
+# sudo pip install paramiko PyYAML jinja2 httplib2
+
+git clone git@github.com:MarkBorcherding/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+git co ansible
